158fe182850d846319b21d7411890f039dbc4fdf
1F10230197 <s1F102301976@iniad.org>
2024-07-04 11:28:15 +0900

    reruirements.txt追加3

--- todo/tests.py
+++ todo/tests.py
@@ -56,7 +56,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 1)
+        self.assertEqual(len(response.context['tasks']), 0)
 
     
     def test_index_get_order_post(self):
@@ -69,7 +69,7 @@ class TodoViewTestCase(TestCase):
     
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task1)
+        self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
     
 
